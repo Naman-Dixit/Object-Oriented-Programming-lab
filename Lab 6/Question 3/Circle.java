7a69a96be7470d0a8815291abe26ecb9b696b980
Naman Dixit <149253093+Naman-Dixit@users.noreply.github.com>
2025-06-17 20:41:19 +0530

    Create Circle.java

---
+++ Lab 6/Question 3/Circle.java
@@ -0,0 +1,6 @@
+public class Circle extends MathConstants {
+    public void calculateArea(double radius) {
+        double area = PI * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}
